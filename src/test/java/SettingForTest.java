fdead7e4ad95ac40e13b70fdb7fe9f98aa96c592
Kolesnikov Vladislav <kolesnikov@aqsi.ru>
2023-03-31 20:06:21 +0300

    added property for baseUrl, remote

--- src/test/java/SettingForTest.java
+++ src/test/java/SettingForTest.java
@@ -7,11 +7,11 @@ import java.util.Map;
 public class SettingForTest {
     @BeforeAll
     public static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = System.getProperty("browser_size", "1600x1200"); //"1600x1200"; //1680x1050 1600x1200
-        Configuration.browser = System.getProperty("browser", "chrome");//"chrome";
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");//"100.0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browserSize = System.getProperty("browser_size", "1600x1200");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.remote = System.getProperty("remote_adres", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
